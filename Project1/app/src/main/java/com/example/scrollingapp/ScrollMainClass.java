092a415d7c11a33af807d58a7986c10887247219
Suyash Patil <suyap18@gmail.com>
2021-11-11 11:21:20 +0530

    inner class UserClass created with constructor and getter setters

--- Project1/app/src/main/java/com/example/scrollingapp/ScrollMainClass.java
+++ Project1/app/src/main/java/com/example/scrollingapp/ScrollMainClass.java
@@ -1,6 +1,7 @@
 package com.example.scrollingapp;
 
 public class ScrollMainClass {
+    private User userdata;
     String id;
     String likes;
 
@@ -26,5 +27,41 @@ public class ScrollMainClass {
 
     public void setLikes(String likes) {
         this.likes = likes;
+    }
+    public void setUser(String id,String name){
+        this.userdata=new User(id,name);
+    }
+    public User getUser(){
+        return userdata;
+    }
+
+
+    public class User{
+        String id;
+        String name;
+
+        public User() {
+        }
+
+        public User(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
