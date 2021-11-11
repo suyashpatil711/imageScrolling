885f5872bdce37926e6f98ee24babfdbaf0063cf
Suyash Patil <suyap18@gmail.com>
2021-11-11 10:32:53 +0530

    Layout done

--- Project1/app/src/main/java/com/example/scrollingapp/MainActivity.java
+++ Project1/app/src/main/java/com/example/scrollingapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+        private static String json_url ="https://pastebin.com/raw/wgkJgazE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
